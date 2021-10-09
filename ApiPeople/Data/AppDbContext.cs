c7cc87b0f0a9df482353a3fda5f52197cb786d0a
Andrea <a2020122545@estudiantes.upsa.edu.bo>
2021-10-09 09:39:01 -0400

    Agregar archivos de proyecto.

---
+++ ApiPeople/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiPeople.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPeople.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<Person> People { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)// enlaza nuestra base de datos con el EntityFramework
+        {
+
+        }
+    }
+}
---
+++ WebPeople/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebPeople.Models;
+
+namespace WebPeople.Data
+{
+    public class AppDbContext: DbContext
+    {
+
+        public DbSet<Person> People { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)// enlaza nuestra base de datos con el EntityFramework
+        {
+
+        }
+    }
+}
